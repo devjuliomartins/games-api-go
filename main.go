89647402a0d79b7b1de941d18ab1965188e0ac46
Julio Martins <contatoeujuliomartins@gmail.com>
2025-04-11 00:11:23 -0300

    Primeira rota e testando primeiro game

--- main.go
+++ main.go
@@ -2,7 +2,21 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func ExibirGames(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":             "1",
+		"Titulo":         "CS:GO",
+		"Genero":         "FPS",
+		"AnoLancamento":  "2012",
+		"Desenvolvedora": "Valve",
+		"Nota":           "9.0",
+	})
+}
+
 func main() {
 	r := gin.Default()
-	r.Run()
+
+	r.GET("/games", ExibirGames)
+
+	r.Run(":8080")
 }
