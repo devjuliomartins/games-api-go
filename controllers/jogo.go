fc29184f0b4f05f5709fe941b876e1b94de301aa
Julio Martins <contatoeujuliomartins@gmail.com>
2025-04-17 00:47:51 -0300

    refatora o ListarJogos para o uso da Paginação modularizada

--- controllers/jogo.go
+++ controllers/jogo.go
@@ -1,68 +1,25 @@
 package controllers
 
 import (
-	"fmt"
+	"github.com/devjuliomartins/games-api-go/Paginacao/controller"
 	"github.com/devjuliomartins/games-api-go/database"
 	"github.com/devjuliomartins/games-api-go/models"
 	"github.com/gin-gonic/gin"
-	"math"
 	"net/http"
-	"strconv"
 )
 
-type PaginationMeta struct {
-	TotalJogos   int `json:"total_jogos"`
-	Limite       int `json:"limite"`
-	PaginaAtual  int `json:"pagina_atual"`
-	TotalPaginas int `json:"total_paginas"`
-}
-
-func GetPaginationParams(c *gin.Context) (int, int, int, error) {
-	paginaStr := c.DefaultQuery("pagina", "1")
-	limiteStr := c.DefaultQuery("limite", "10")
-
-	pagina, err := strconv.Atoi(paginaStr)
-	if err != nil || pagina < 1 {
-		return 0, 0, 0, fmt.Errorf("invalid 'pagina' parameter")
-	}
-
-	limite, err := strconv.Atoi(limiteStr)
-	if err != nil || limite < 1 {
-		return 0, 0, 0, fmt.Errorf("invalid 'limite' parameter")
-	}
-
-	offset := (pagina - 1) * limite
-	return pagina, limite, offset, nil
-}
-
 func ListarJogos(c *gin.Context) {
-	pagina, limite, offset, err := GetPaginationParams(c)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
-		return
-	}
-
-	var total_jogos int64
-	if err := database.DB.Model(&models.Jogo{}).Count(&total_jogos).Error; err != nil {
-		c.JSON(500, gin.H{"error": "error counting games"})
-		return
-	}
-
 	var jogos []models.Jogo
-	if err := database.DB.Offset(offset).Limit(limite).Find(&jogos).Error; err != nil {
-		c.JSON(500, gin.H{"error": "error fetching games"})
+	query := database.DB.Model(&models.Jogo{})
+
+	meta, err := controller.PaginarConsulta(c, query, &jogos)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	total_paginas := int(math.Ceil(float64(total_jogos) / float64(limite)))
 
 	c.JSON(200, gin.H{
-		"meta": PaginationMeta{
-			TotalJogos:   int(total_jogos),
-			Limite:       limite,
-			PaginaAtual:  pagina,
-			TotalPaginas: total_paginas,
-		},
+		"meta": meta,
 		"data": jogos,
 	})
 }
